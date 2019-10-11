8641c1a1bfb4ab44ac6bfb957e761f5650b0df98
Gustav Rasmussen <gcr84@hotmail.com>
2019-10-11 12:55:05 +0200

    Update scriupt

--- scripts/test_geometry.py
+++ scripts/test_geometry.py
@@ -10,8 +10,9 @@ import geometry
 
 
 def test_rectangle_slopes():
-    assert (2.747477419454621, 2.747477419454621, -0.3639702342662024,
-            -0.3639702342662024) == geometry.rectangle_slopes(math.radians(70))
+    angle = math.radians(70)
+    slopes = (2.747477419454621, 2.747477419454621, -0.3639702342662024, -0.3639702342662024)
+    assert slopes == geometry.rectangle_slopes(angle), f"Should be {slopes}"
 
 
 def test_intersect():
@@ -19,5 +20,12 @@ def test_intersect():
 
 
 def test_find_point_on_line():
-    assert (5.47213595499958, 3.23606797749979) == geometry.find_point_on_line(1, 1, 5, .5)
+    point_on_line = (5.47213595499958, 3.23606797749979)
+    assert point_on_line == geometry.find_point_on_line(1, 1, 5, .5), f"Should be {point_on_line}"
 
+
+if __name__ == "__main__":
+    test_rectangle_slopes()
+    test_intersect()
+    test_find_point_on_line()
+    print("Everything passed")
