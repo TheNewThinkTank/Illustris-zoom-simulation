25e89cce73a723c23f94a1a7a9c4d4caaf498ab7
Gustav Rasmussen <gcr84@hotmail.com>
2024-12-21 15:53:07 +0100

    refactor: rename dirs. split sfr script in 2. remove circleci. update pyproject.toml

--- test/test_rectangle.py
+++ test/test_rectangle.py
@@ -1,35 +1,28 @@
-"""
-Unit tests for the geometry program
-"""
 
-# Standard library
 import math
-from typing import Final, Tuple
-
-# Local application
-import geometry
+from typing import Final
+import src.rectangle as rectangle
 
 
 def test_rectangle_slopes():
     angle: Final = math.radians(70)
-    slopes: Tuple = (2.747477419454621,
+    slopes: tuple = (2.747477419454621,
                      2.747477419454621,
                      -0.3639702342662024,
                      -0.3639702342662024
                      )
-    assert geometry.rectangle_slopes(angle) == slopes, f"Should be {slopes}"
+    assert rectangle.rectangle_slopes(angle) == slopes, f"Should be {slopes}"
 
 
 def test_intersect():
     intersection: int = 10.5
-    assert geometry.intersect(.5, 3, 12) == intersection, f"Should be {intersection}"
+    assert rectangle.intersect(.5, 3, 12) == intersection, f"Should be {intersection}"
 
 
 def test_find_point_on_line():
-    point_on_line: Tuple = (5.47213595499958, 3.23606797749979)
-    assert geometry.find_point_on_line(1,
+    point_on_line: tuple = (5.47213595499958, 3.23606797749979)
+    assert rectangle.find_point_on_line(1,
                                        1,
                                        5,
                                        .5) == point_on_line, \
                                         f"Should be {point_on_line}"
-
